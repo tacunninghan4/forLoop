6a966cdccb88a29a6d2e0c154bde2020a0325cfc
Todd <andrewcunningham770@gmail.com>
2021-07-20 05:13:27 -0400

    bye

---
+++ js index.js
@@ -0,0 +1,30 @@
+var ourArray = [];
+var i = 0;
+while(i < 5) {
+  ourArray.push(i);
+  i++;
+}
+  counsole.log(ourArray);
+
+
+var ourArray = [];
+var i = 0;
+while(i < 10) {
+  ourArray.push(i);
+  i++;
+}
+  counsole.log(ourArray);
+
+
+  var ourArray = [];
+  for (var i = 1; i < 10; i += 2) {
+    ourArray.push(i);
+  }
+console.log(ourArray);
+
+var ourArray = [];
+for (var i = 15; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+console.log(ourArray);
+
